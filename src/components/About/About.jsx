556002e7ee1b7219cda2bb954cb106a96040171f
Frankie <frankie.m.18@hotmail.com>
2025-04-07 22:47:10 -0700

    built out card added images and a button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,32 @@
 import "./About.scss";
+import Kaya from "../../assets/images/kaya-intro.jpg";
+import Hat from "../../assets/images/hat.jpg";
+import Landing from "../../assets/images/landingpage-desktop.png";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <ul className="about__list">
+        <li className="about__item">
+          <h4 className="about__title"></h4>
+          <img src={Kaya} alt="" className="about__image" />
+          <p className="about__button">3D Animation</p>
+        </li>
+        <li className="about__item">
+          {" "}
+          <h4 className="about__title"></h4>
+          <img src={Hat} alt="" className="about__image" />
+          <p className="about__button">Beadwork</p>
+        </li>
+        <li className="about__item">
+          {" "}
+          <h4 className="about__title"></h4>
+          <img src={Landing} alt="" className="about__image" />
+          <p className="about__button">Web Development</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default About;
