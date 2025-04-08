441d42a14207b93dbd27dac260d4fc1be9f8eb06
Frankie <frankie.m.18@hotmail.com>
2025-04-07 18:04:05 -0700

    initial header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
 import "./Header.scss";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <ul className="header">
+        <li className="header__item">Icon</li>
+        <li className="header__item">About</li>
+        <li className="header__item">Animation</li>
+        <li className="header__item">Web Development</li>
+        <li className="header__item">Contact</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
