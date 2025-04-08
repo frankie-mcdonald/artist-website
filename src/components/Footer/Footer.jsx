4ee37a4de305c9116becdbb53dcd0798d0429c39
Frankie <frankie.m.18@hotmail.com>
2025-04-07 23:07:09 -0700

    hero adjust

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import "./Footer.scss";
+import GitHub from "../../assets/icons/github.png";
+import LinkedIn from "../../assets/icons/linkedin.png";
+import Vimeo from "../../assets/icons/vimeo.png";
+import Youtube from "../../assets/icons/youtube.png";
 
 function Footer() {
   return (
@@ -9,6 +13,20 @@ function Footer() {
         <li className="footer__item">Volunteer work</li>
         <li className="footer__item">Portfolio</li>
         <li className="footer__item">Copyright Frankie McDonald 2025</li>
+      </ul>
+      <ul className="header__icon-list">
+        <li className="header__icon-item">
+          <img className="header__icon" src={GitHub} alt="" />
+        </li>
+        <li className="header__icon-item">
+          <img className="header__icon" src={LinkedIn} alt="" />
+        </li>
+        <li className="header__icon-item">
+          <img className="header__icon" src={Vimeo} alt="" />
+        </li>
+        <li className="header__icon-item">
+          <img className="header__icon" src={Youtube} alt="" />
+        </li>
       </ul>
     </div>
   );
