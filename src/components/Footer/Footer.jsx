b5ae765ed2e59c995ae6387f9ea4522c49460e39
Frankie <frankie.m.18@hotmail.com>
2025-04-07 22:51:34 -0700

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h2 className="footer__title">Frankie McDonald</h2>
+    </div>
+  );
+}
+
+export default Footer;
