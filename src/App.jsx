9ce98198161316bdc6481b417ff060890829df98
Frankie <frankie.m.18@hotmail.com>
2025-04-07 22:57:42 -0700

    intial footer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.scss";
 import About from "./components/About/About.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,12 @@ function Footer() {
   return (
     <div className="footer">
       <h2 className="footer__title">Frankie McDonald</h2>
+      <ul className="footer__list">
+        <li className="footer__item">Contact</li>
+        <li className="footer__item">Volunteer work</li>
+        <li className="footer__item">Portfolio</li>
+        <li className="footer__item">Copyright Frankie McDonald 2025</li>
+      </ul>
     </div>
   );
 }
